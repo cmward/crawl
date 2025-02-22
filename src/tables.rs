7425737d0643c35b3d1dddd4a84a6751800debd1
Chris Ward <chrisward.cmw@gmail.com>
2025-02-22 11:19:54 -0500

    Table from csv

--- src/tables.rs
+++ src/tables.rs
@@ -1,61 +1,93 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, error::Error, fs::File};
 
 use crate::{dice::DiceRoll, error::CrawlError, rolls::RollTarget};
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub struct TableEntry {
     roll_target: RollTarget,
     value: String,
 }
 
 #[derive(Debug, PartialEq)]
-pub struct TableRollResult<'a> {
-    entry: &'a TableEntry,
+pub struct TableRollResult {
+    entry: TableEntry, // should be a reference?
 }
 
-impl<'a> TableRollResult<'a> {
-    pub fn new(entry: &'a TableEntry) -> Self {
+impl TableRollResult {
+    pub fn new(entry: TableEntry) -> Self {
         Self { entry }
     }
 }
 
 #[derive(Debug)]
-pub struct Table<'a> {
-    entries: HashMap<i32, &'a TableEntry>,
+pub struct Table {
+    entries: Vec<TableEntry>,
+    roll_targets: HashMap<i32, usize>,
 }
 
-impl<'a> Table<'a> {
-    pub fn new(entries: HashMap<i32, &'a TableEntry>) -> Self {
-        Table { entries }
+impl Table {
+    pub fn new(entries: Vec<TableEntry>, roll_targets: HashMap<i32, usize>) -> Self {
+        Table {
+            entries,
+            roll_targets,
+        }
     }
 
     pub fn roll(&self, dice: DiceRoll) -> Result<TableRollResult, CrawlError> {
         let roll_result = dice.roll();
-        if let Some(selected_entry) = self.entries.get(&roll_result.total) {
-            Ok(TableRollResult::new(selected_entry))
+        if let Some(entry_idx) = self.roll_targets.get(&roll_result.total) {
+            Ok(TableRollResult::new(
+                self.entries.get(*entry_idx).unwrap().clone(), // TODO: reference
+            ))
         } else {
             Err(CrawlError::InterpreterError {
                 reason: format!("roll {roll_result:?} not a valid index for table"),
             })
         }
     }
+
+    pub fn load(filepath: &str) -> Result<Self, Box<dyn Error>> {
+        let file = File::open(filepath)?;
+        let mut rdr = csv::ReaderBuilder::new()
+            .has_headers(false)
+            .from_reader(file);
+
+        let mut entries = Vec::new();
+        for result in rdr.records() {
+            let record = result?;
+            let roll_target = record.get(0).unwrap();
+            let value = record.get(1).unwrap();
+            let entry = TableEntry {
+                roll_target: String::from(roll_target).try_into().unwrap(),
+                value: value.into(),
+            };
+            entries.push(entry);
+        }
+
+        Ok(Self::from(entries))
+    }
 }
 
-impl<'a> From<Vec<&'a TableEntry>> for Table<'a> {
-    fn from(value: Vec<&'a TableEntry>) -> Self {
-        let mut entries = HashMap::<i32, &'a TableEntry>::new();
-        for entry in value {
-            let idxs = match entry.roll_target {
+impl From<Vec<TableEntry>> for Table {
+    fn from(value: Vec<TableEntry>) -> Self {
+        let mut entries = Vec::new();
+        let mut roll_targets = HashMap::<i32, usize>::new();
+        for (idx, entry) in value.into_iter().enumerate() {
+            let entry_roll_targets = match entry.roll_target {
                 RollTarget::Num(n) => vec![n],
                 RollTarget::NumRange(n, m) => (n..=m).collect(),
             };
+            entries.push(entry);
 
-            for idx in idxs {
-                entries.insert(idx, entry);
+            for roll_target in entry_roll_targets {
+                roll_targets.insert(roll_target, idx);
             }
         }
 
-        Table { entries }
+        Table {
+            entries,
+            roll_targets,
+        }
     }
 }
 
@@ -75,19 +107,19 @@ mod tests {
             roll_target: RollTarget::NumRange(7, 12),
             value: "7-12".into(),
         };
-        let table = dbg!(Table::from(vec![&low_entry, &high_entry]));
+        let table = Table::from(vec![low_entry.clone(), high_entry.clone()]);
 
-        let dice = dbg!(DiceRoll::new(DicePool::new(vec![Die(1)]), 0));
-        let result = dbg!(table.roll(dice).unwrap());
-        assert_eq!(result, TableRollResult { entry: &low_entry });
+        let dice = DiceRoll::new(DicePool::new(vec![Die(1)]), 0);
+        let result = table.roll(dice).unwrap();
+        assert_eq!(result, TableRollResult { entry: low_entry });
 
         let dice = DiceRoll::new(DicePool::new(vec![Die(1)]), 11);
         let result = table.roll(dice).unwrap();
-        assert_eq!(result, TableRollResult { entry: &high_entry });
+        assert_eq!(result, TableRollResult { entry: high_entry });
     }
 
     #[test]
-    fn roll_num_target() {
+    fn num_target_table_from_vec() {
         let zero_entry = TableEntry {
             roll_target: RollTarget::Num(0),
             value: "zero".into(),
@@ -97,11 +129,25 @@ mod tests {
             value: "one".into(),
         };
 
-        let table = Table::new(HashMap::from([(0, &zero_entry), (1, &one_entry)]));
+        let table = Table::from(vec![zero_entry.clone(), one_entry.clone()]);
 
         let dice = DiceRoll::new(DicePool::new(vec![Die(1)]), 0);
         let result = table.roll(dice).unwrap();
 
-        assert_eq!(result, TableRollResult { entry: &one_entry });
+        assert_eq!(result, TableRollResult { entry: one_entry });
+    }
+
+    #[test]
+    fn from_csv() {
+        let table = dbg!(Table::load("table.csv").unwrap());
+
+        let dice = DiceRoll::new(DicePool::new(vec![Die(1)]), 11);
+        let result = table.roll(dice).unwrap();
+
+        let entry = TableEntry {
+            roll_target: RollTarget::NumRange(7, 12),
+            value: "total darkness and dread".into(),
+        };
+        assert_eq!(result, TableRollResult { entry })
     }
 }
