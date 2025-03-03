44448b5d7c8739c7a489ae0947f4afc82285bdcc
Chris <cmward@users.noreply.github.com>
2025-03-02 21:43:16 -0500

    OverOrEqual roll target (#14)

--- src/tables.rs
+++ src/tables.rs
@@ -27,30 +27,57 @@ impl<'a> TableRollResult<'a> {
 pub struct Table {
     entries: Vec<TableEntry>,
     roll_targets: HashMap<i32, usize>,
+    min_target: i32,
+    max_target: i32,
+    clamp_to_min: bool,
+    clamp_to_max: bool,
 }
 
 impl Table {
     pub fn new(entries: Vec<TableEntry>, roll_targets: HashMap<i32, usize>) -> Self {
+        let min_target = *roll_targets.keys().min().unwrap();
+        let max_target = *roll_targets.keys().max().unwrap();
         Table {
             entries,
             roll_targets,
+            min_target,
+            max_target,
+            clamp_to_min: true,
+            clamp_to_max: true,
+        }
+    }
+
+    fn get_value_for_target(&self, target: &i32) -> Result<TableRollResult, CrawlError> {
+        if let Some(entry_idx) = self.roll_targets.get(target) {
+            Ok(TableRollResult::new(self.entries.get(*entry_idx).unwrap()))
+        } else {
+            Err(CrawlError::InterpreterError {
+                reason: format!("target {target:?} not a valid index for table"),
+            })
         }
     }
 
     pub fn roll(&self, dice: &DiceRoll) -> Result<TableRollResult, CrawlError> {
         let roll_result = dice.roll();
-        if let Some(entry_idx) = self.roll_targets.get(&roll_result.total) {
-            Ok(TableRollResult::new(self.entries.get(*entry_idx).unwrap()))
-        } else {
-            Err(CrawlError::InterpreterError {
-                reason: format!("roll {roll_result:?} not a valid index for table"),
-            })
+        let roll_value = self.roll_targets.get(&roll_result.total);
+        match roll_value {
+            Some(entry_idx) => Ok(TableRollResult::new(self.entries.get(*entry_idx).unwrap())),
+            None => {
+                if roll_result.total < self.min_target && self.clamp_to_min {
+                    return self.get_value_for_target(&self.min_target);
+                } else if roll_result.total > self.max_target && self.clamp_to_max {
+                    return self.get_value_for_target(&self.max_target);
+                } else {
+                    Err(CrawlError::InterpreterError {
+                        reason: format!("roll {roll_result:?} not a valid index for table"),
+                    })
+                }
+            }
         }
     }
 
     pub fn auto_roll(&self) -> Result<TableRollResult, CrawlError> {
-        let max_value = *self.roll_targets.keys().max().unwrap();
-        let dice = vec![Die(max_value)];
+        let dice = vec![Die(self.max_target)];
         let dice_pool = DicePool::new(dice);
         let roll = DiceRoll::new(dice_pool, 0);
         self.roll(&roll)
@@ -69,7 +96,7 @@ impl Table {
             let roll_target = record.get(0).unwrap();
             let value = record.get(1).unwrap();
             let entry = TableEntry {
-                roll_target: String::from(roll_target).try_into().unwrap(),
+                roll_target: roll_target.try_into().unwrap(),
                 value: value.into(),
             };
             entries.push(entry);
@@ -87,6 +114,7 @@ impl From<Vec<TableEntry>> for Table {
             let entry_roll_targets = match entry.roll_target {
                 RollTarget::Num(n) => vec![n],
                 RollTarget::NumRange(n, m) => (n..=m).collect(),
+                RollTarget::OverOrEqual(n) => vec![n],
             };
             entries.push(entry);
 
@@ -95,10 +123,7 @@ impl From<Vec<TableEntry>> for Table {
             }
         }
 
-        Table {
-            entries,
-            roll_targets,
-        }
+        Self::new(entries, roll_targets)
     }
 }
 
@@ -149,6 +174,44 @@ mod tests {
     }
 
     #[test]
+    fn under_min_target() {
+        let zero_entry = TableEntry {
+            roll_target: RollTarget::Num(0),
+            value: "zero".into(),
+        };
+        let one_entry = TableEntry {
+            roll_target: RollTarget::Num(1),
+            value: "one".into(),
+        };
+
+        let table = Table::from(vec![zero_entry.clone(), one_entry.clone()]);
+
+        let dice = DiceRoll::new(DicePool::new(vec![Die(1)]), -100);
+        let result = table.roll(&dice).unwrap();
+
+        assert_eq!(result, TableRollResult { entry: &zero_entry });
+    }
+
+    #[test]
+    fn over_max_target() {
+        let zero_entry = TableEntry {
+            roll_target: RollTarget::Num(0),
+            value: "zero".into(),
+        };
+        let one_entry = TableEntry {
+            roll_target: RollTarget::Num(1),
+            value: "one".into(),
+        };
+
+        let table = Table::from(vec![zero_entry.clone(), one_entry.clone()]);
+
+        let dice = DiceRoll::new(DicePool::new(vec![Die(1)]), 100);
+        let result = table.roll(&dice).unwrap();
+
+        assert_eq!(result, TableRollResult { entry: &one_entry });
+    }
+
+    #[test]
     fn from_csv() {
         // TODO: test fixtures location
         let table = Table::load("table.csv").unwrap();
@@ -159,6 +222,20 @@ mod tests {
         let entry = TableEntry {
             roll_target: RollTarget::NumRange(7, 12),
             value: "total darkness and dread".into(),
+        };
+        assert_eq!(result, TableRollResult { entry: &entry });
+    }
+
+    #[test]
+    fn over_target_from_csv() {
+        let table = Table::load("table.csv").unwrap();
+
+        let dice = DiceRoll::new(DicePool::new(vec![Die(1)]), 100);
+        let result = table.roll(&dice).unwrap();
+
+        let entry = TableEntry {
+            roll_target: RollTarget::OverOrEqual(13),
+            value: "fog seeps up through the dirt".into(),
         };
         assert_eq!(result, TableRollResult { entry: &entry })
     }
